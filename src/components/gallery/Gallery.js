f12d022892a3d6e3b9b5c5b56e572a0690a88742
MrUnwonted <arjungodzarjun@gmail.com>
2024-12-19 18:29:31 +0530

    Gallery Changed

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -1,28 +1,77 @@
-import React from 'react'
-import './Gallery.css'
+import React from "react";
+import "./Gallery.css";
+import Water from "../../assest/water.jpg";
+import Innovations from "../../assest/Innovations.webp";
+import Refreshing_the_planet from "../../assest/Refreshing_the_planet.webp";
+import Adding_value_accross_sectors from "../../assest/Adding_value_accross_sectors.png";
+import wastewater_consultancy from "../../assest/wastewater_consultancy.jpg";
+import water_lab_testing from "../../assest/industrial_boiler_cleaning.avif";
+import water_services from "../../assest/water_services.webp";
+import industrial_boiler_cleaning from "../../assest/industrial_boiler_cleaning.avif";
 
 export default function Gallery() {
-    return (
-        <div className='about'>
-            <div className="album">
-                <div className='abtxt'>Visit our Gallery</div>
-                <div className="responsive-container-block bg">
-                    <div className="responsive-container-block img-cont">
-                        <img className="img" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/PP5.4.svg" />
-                        <img className="img" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/PP5.5.svg" />
-                        <img className="img img-last" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/PP5.6.svg" />
-                    </div>
-                    <div className="responsive-container-block img-cont">
-                        <img className="img img-big" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/PP5.11.svg" />
-                        <img className="img img-big img-last" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/PP5.10.svg" />
-                    </div>
-                    <div className="responsive-container-block img-cont">
-                        <img className="img" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/PP5.7.svg" />
-                        <img className="img" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/PP5.8.svg" />
-                        <img className="img" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/PP5.9.svg" />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="about">
+      <div className="album">
+        <div className="abtxt">Visit our Gallery</div>
+        
+        <div className="responsive-container-block bg">
+          <div className="responsive-container-block img-cont">
+            <img
+              className="img"
+              loading="lazy"
+              alt="WaterImage"
+              src={Water}
+            />
+            <img
+              className="img"
+              loading="lazy"
+              alt="WaterImage"
+              src={wastewater_consultancy}
+            />
+            <img
+              className="img"
+              loading="lazy"
+              alt="WaterImage"
+              src={water_lab_testing}
+            />
+          </div>
+          <div className="responsive-container-block img-cont">
+          <img
+              className="img"
+              loading="lazy"
+              alt="WaterImage"
+              src={Refreshing_the_planet}
+            />
+            <img
+              className="img"
+              loading="lazy"
+              alt="WaterImage"
+              src={Adding_value_accross_sectors}
+            />
+          </div>
+          <div className="responsive-container-block img-cont">
+          <img
+              className="img"
+              loading="lazy"
+              alt="WaterImage"
+              src={Innovations}
+            />
+           <img
+              className="img"
+              loading="lazy"
+              alt="WaterImage"
+              src={water_services}
+            />
+            <img
+              className="img"
+              loading="lazy"
+              alt="WaterImage"
+              src={industrial_boiler_cleaning}
+            />
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
